8f1993e53644cd6004372554fec82b7fa82d655d
buysdb <i@buysdb.nl>
2019-05-15 21:43:02 +0200

    Added installation requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 setup(
-	name='preciseControl',
+	name='sparsebinarydistance',
 	version='0.01',
 	author='Buys de Barbanson',
 	author_email='code@buysdb.nl',
 	description='Calculate a distance matrix based on a binary feature matrix with missing data ',
 	url='https://github.com/BuysDB/SparseBinaryDistance',
-	py_modules=['sparsebinarydistance
-'],
-	install_requires=['pygame']
+	py_modules=['sparsebinarydistance'],
+	install_requires=['pandas','numpy']
 )
